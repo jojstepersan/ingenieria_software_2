847dfb6e236a2c46dfc7a929f888ebc7251b464c
Cesar Rojas <mitzyliliana.beltran.contractor@bbva.com.co>
2018-02-19 17:31:18 -0500

    ajustes en el front de angular ajuste css

--- FunnyPetsFront/src/app/app.component.ts
+++ FunnyPetsFront/src/app/app.component.ts
@@ -3,19 +3,19 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css',
-		'../assets/css/animate.css',
-		'../assets/css/bootstrap-datepicker.min.css',
-		'../assets/css/bootstrap-datetimepicker.min.css',
-		'../assets/css/flexslider.css',
-		'../assets/css/icomoon.css',
-		'../assets/css/magnific-popup.css',
-		'../assets/css/owl.carousel.min.css',
-		'../assets/css/owl.theme.default.min.css',
-		'../assets/css/icomoon.css',
-		'../assets/css/themify-icons.css',
-		'../assets/css/magnific-popup.css',
-		'../assets/css/style.css'
+    styleUrls: ['./app.component.css'
+		// '../assets/css/animate.css',
+		// '../assets/css/bootstrap-datepicker.min.css',
+		// '../assets/css/bootstrap-datetimepicker.min.css',
+		// '../assets/css/flexslider.css',
+		// '../assets/css/icomoon.css',
+		// '../assets/css/magnific-popup.css',
+		// '../assets/css/owl.carousel.min.css',
+		// '../assets/css/owl.theme.default.min.css',
+		// '../assets/css/icomoon.css',
+		// '../assets/css/themify-icons.css',
+		// '../assets/css/magnific-popup.css',
+		// '../assets/css/style.css'
 	       ]
 })
 export class AppComponent {
