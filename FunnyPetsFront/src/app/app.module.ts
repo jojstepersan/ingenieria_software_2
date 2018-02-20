882614f13d1efb97fd9471a50c564e90108e4035
0rphen <the.bebop.88@gmail.com>
2018-02-20 01:27:22 -0500

    Se trabaja con el llamado a firebase adaptando la parte backend Comienza a trabajarse con la parte dedicada a angular con integración en firebase, se eliminan objetos innecesarios, estilos y demás archivos ajustados para implementar el template.

--- FunnyPetsFront/src/app/app.module.ts
+++ FunnyPetsFront/src/app/app.module.ts
@@ -2,19 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { APP_ROUTING } from './app.routes';
 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { NavComponent } from './components/share/nav/nav.component';
+import { FooterComponent } from './components/share/footer/footer.component';
+import { HeaderComponent } from './components/share/header/header.component';
+
+import { UsersService } from './services/users.service';
 
 @NgModule({
     declarations: [
         AppComponent,
-        LoginComponent
+        LoginComponent,
+        NavComponent,
+        FooterComponent,
+        HeaderComponent
     ],
     imports: [
+        FormsModule,
+        HttpModule,
         BrowserModule,
         APP_ROUTING
     ],
-    providers: [],
+    providers: [UsersService],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
--- FunnyPetsFront/src/app/components/login/login.component.ts
+++ FunnyPetsFront/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../interfaces/user';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
@@ -6,24 +8,33 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+    user: User = {
+        email: '',
+        password: '',
+        birthday: '',
+        key$: ''
+    }
     constructor() { }
 
     ngOnInit() {
-	this.loadScript('assets/js/jquery.easing.1.3.js');
-	this.loadScript('assets/js/jquery.waypoints.min.js');
-	this.loadScript('assets/js/owl.carousel.min.js');
-	this.loadScript('assets/js/moment.min.js');
-	this.loadScript('assets/js/jquery.countTo.js');
-	this.loadScript('assets/js/bootstrap-datetimepicker.min.js');
-	this.loadScript('assets/js/bootstrap-datepicker.min.js');
-	this.loadScript('assets/js/main.js');
+        this.loadScript('assets/js/jquery.easing.1.3.js');
+        this.loadScript('assets/js/jquery.waypoints.min.js');
+        this.loadScript('assets/js/owl.carousel.min.js');
+        this.loadScript('assets/js/moment.min.js');
+        this.loadScript('assets/js/jquery.countTo.js');
+        this.loadScript('assets/js/bootstrap-datetimepicker.min.js');
+        this.loadScript('assets/js/bootstrap-datepicker.min.js');
+        this.loadScript('assets/js/main.js');
     }
 
     public loadScript(url) {
-	let node = document.createElement('script');
-	node.src = url;
-	node.type = 'text/javascript';
-	document.getElementsByTagName('head')[0].appendChild(node);
+        let node = document.createElement('script');
+        node.src = url;
+        node.type = 'text/javascript';
+        document.getElementsByTagName('head')[0].appendChild(node);
+    }
+
+    guardar() {
+        console.log(this.user);
     }
 }
